135dd85bf3494c8697bd76cecb6a36f12b562e2d
dolininak <144163170+dolininak@users.noreply.github.com>
2024-05-10 20:32:59 +0300

    Add files via upload

--- delete.php
+++ delete.php
@@ -5,6 +5,12 @@ if (isset($_GET['id'])) {
     include ('conf3.php');
     $db = new PDO('mysql:host=localhost;dbname=u67432', $user, $pass,
     [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+    $stmt1 = $pdo->prepare("DELETE FROM application_programming_language WHERE application_id = :id");
+    $stmt1->bindParam(':id', $id);
+    $stmt1->execute();
+    
+
     $stmt = $db->prepare("DELETE FROM application WHERE id = :id");
     $stmt->bindParam(':id', $id);
     $stmt->execute();
