5b9707f7df63e8bc0a31a539a9475c6639bf7a83
dolininak <144163170+dolininak@users.noreply.github.com>
2024-05-11 01:13:52 +0300

    Add files via upload

--- admin.php
+++ admin.php
@@ -82,9 +82,9 @@ $stmt->execute();
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-foreach ($results as $row) {
-    echo $row['language'] . ' has ' . $row['total_lovers'] . ' lovers.' . PHP_EOL;
-}
+
+    echo $results['language'] . ' has ' . $results['total_lovers'] . ' lovers.' . PHP_EOL;
+
 
 
 // *********
