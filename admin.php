11d352eb782f79b2f5eca9fa4d1ad7d1c9960e2f
dolininak <144163170+dolininak@users.noreply.github.com>
2024-05-10 17:58:14 +0300

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Задача 6. Реализовать вход администратора с использованием
+ * HTTP-авторизации для просмотра и удаления результатов.
+ **/
+
+// Пример HTTP-аутентификации.
+// PHP хранит логин и пароль в суперглобальном массиве $_SERVER.
+// Подробнее см. стр. 26 и 99 в учебном пособии Веб-программирование и веб-сервисы.
+include ('conf3.php');
+$db = new PDO('mysql:host=localhost;dbname=u67432', $user, $pass,
+  [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+// Запрос к таблице Admins
+$stmt = $db->prepare("SELECT login, password FROM Admins WHERE login = 'admin'");
+$stmt->execute();
+$row = $stmt->fetch();
+
+$login = $row['login'];
+$hashedPassword = $row['password'];
+if (empty($_SERVER['PHP_AUTH_USER']) ||
+    empty($_SERVER['PHP_AUTH_PW']) ||
+    $_SERVER['PHP_AUTH_USER'] != $login ||
+    md5($_SERVER['PHP_AUTH_PW']) != $hashedPassword) {
+  header('HTTP/1.1 401 Unanthorized');
+  header('WWW-Authenticate: Basic realm="My site"');
+  print('<h1>401 Требуется авторизация</h1>');
+  exit();
+}
+
+print('Вы успешно авторизовались и видите защищенные паролем данные.');
+include ('conf3.php');
+    $db = new PDO('mysql:host=localhost;dbname=u67432', $user, $pass,
+    [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+try {
+$stmt = $db->prepare("SELECT * FROM  application");
+$stmt->execute();
+$users = $stmt->fetchAll();
+
+// Отображение данных в виде таблицы
+echo '<table>';
+echo '<tr><th>ID</th><th>Name</th><th>Phone</th><th>Email</th><th>Date</th><th>Gender</th><th>Biografy</th><th>Actions</th></tr>';
+foreach ($users as $user) {
+    echo '<tr>';
+    echo '<td>' . $user['id'] . '</td>';
+    echo '<td>' . $user['name'] . '</td>';
+    echo '<td>' . $user['tel'] . '</td>';
+    echo '<td>' . $user['email'] . '</td>';
+    echo '<td>' . $user['data'] . '</td>';
+    echo '<td>' . $user['pol'] . '</td>';
+    echo '<td>' . $user['bio'] . '</td>';
+    echo '<td><a href="edit.php?id=' . $user['id'] . '">Edit</a> | <a href="delete.php?id=' . $user['id'] . '">Delete</a></td>';
+    echo '</tr>';
+}
+echo '</table>';}
+catch(PDOException $e){
+  print('Error : ' . $e->getMessage());
+  exit();
+}
+// *********
+// Здесь нужно прочитать отправленные ранее пользователями данные и вывести в таблицу.
+// Реализовать просмотр и удаление всех данных.
+// *********
