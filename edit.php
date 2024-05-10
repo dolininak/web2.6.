27486310c0c6c4deac2aaa6c6794ad005ea3aa89
dolininak <144163170+dolininak@users.noreply.github.com>
2024-05-10 22:05:01 +0300

    Add files via upload

--- edit.php
+++ edit.php
@@ -4,36 +4,39 @@ if (isset($_GET['id'])) {
     $db = new PDO('mysql:host=localhost;dbname=u67432', $user, $pass,
     [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
     $id = $_GET['id'];
-    $stmt = $db->prepare("SELECT * FROM application WHERE id = :id");
-    $stmt->bindParam(':id', $id);
+
+    $stmt = $db->prepare("SELECT login, pass FROM application WHERE id = :id");
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
     $stmt->execute();
-    $user = $stmt->fetch();
-     
-    echo '<form action="update.php" method="post">';
-    echo '<input type="hidden" name="id" value="' . $user['id'] . '">';
-    echo 'Name: <input type="text" name="name" value="' . $user['name'] . '"><br>';
-    echo 'Tel: <input type="text" name="tel" value="' . $user['tel'] . '"><br>';
-    echo 'Email: <input type="text" name="email" value="' . $user['email'] . '"><br>';                
-    echo 'Date:<input name="data"  type="date"   placeholder="Введите дату рождения" value="' . $user['data'] . '"> <br>';
-    echo 'Gender:<br>';
-    echo '<input name="pol" type="radio" value="female" ' . ($user['pol'] == 'female' ? 'checked' : '') . '> Женский<br>';
-    echo '<input name="pol" type="radio" value="male" ' . ($user['pol'] == 'male' ? 'checked' : '') . '> Мужской<br>';
-    echo 'Favorite Programming Languages:<br>';
-    echo '<select multiple="multiple" name="languages[]">';
-    $selectedLanguages = explode(',', $user['languages']);
-    $languages = array("Pascal", "C", "C++", "JavaScript", "PHP", "Python", "Java", "Haskell", "Clojure", "Prolog", "Scala");
-    foreach ($languages as $language) {
-        echo '<option value="' . $language . '" ' . (in_array($language, $selectedLanguages) ? 'selected' : '') . '>' . $language . '</option>';
-    }
-    echo '</select><br>';
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    $login = $result['login'];
+    $pass = $result['pass'];
 
-    echo '<textarea placeholder="Ваша биография" name="bio">' . $user['bio'] . '</textarea><br>';
-
-    echo '<input type="checkbox" name="agreement" value="on" ' . ($user['agreement'] == 'on' ? 'checked' : '') . '> Согласен с условиями<br>';              
     
-                       
-    echo '<input type="submit" value="Update">';
-    echo '</form>';
+ include ('conf3.php');
+ $db = new PDO('mysql:host=localhost;dbname=u67432', $user, $pass,
+   [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+ $stmt = $db->prepare("SELECT * FROM application WHERE login = :login AND pass = :password");
+ $stmt->execute(['login' => $login, 'password' => $password]);
+ $user = $stmt->fetch();
+
+ if (!$user) {
+     // Вывод сообщения об ошибке
+     echo "Неверный логин или пароль";
+ } else {
+     if (!$session_started) { 
+         session_start(); 
+     } 
+     // Если все ок, то авторизуем пользователя. 
+     
+     $_SESSION['login'] = $login; 
+     $_SESSION['password'] = $password;
+     // Записываем ID пользователя. 
+     $_SESSION['uid'] = $user['id']; 
+
+     // Делаем перенаправление. 
+     header('Location: ./'); 
+ }
 
 }
 ?>
